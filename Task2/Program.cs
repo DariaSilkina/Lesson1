07cf09fc8b8da872437022afdae8a6c58f40ffa1
Daria Silkina <kuteneva.daria@yandex.ru>
2024-02-19 18:23:06 +0300

    add some example lesson1:Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -17,3 +17,32 @@ double product3 = i1 * d1;
 Console.WriteLine(product1);
 Console.WriteLine(product2);
 Console.WriteLine(product3);
+
+int quotient = i1 / i2;
+Console.WriteLine(quotient);
+
+d1 = 6;
+double quotient2 = i1 / d1;
+Console.WriteLine(quotient2);
+
+
+int a = 5;
+int b = 8;
+int c = 9;
+int d = 10;
+int znak1 = a * b;
+Console.WriteLine(znak1);
+
+int znak2 = c + d;
+int znak3 = znak1 / znak2;
+Console.WriteLine(znak3);
+
+int e = 1;
+int r = 10;
+int t = 2;
+int y = 3;
+int zak1 = e * r;
+int zak2 = t + y;
+int zak3 = zak1 / zak2;
+Console.WriteLine(zak3);
+
